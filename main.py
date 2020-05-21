5b6113ee4188ecb3458ad3d8e60b98598b445352
syntax | tom holland <50985559+syntax@users.noreply.github.com>
2020-05-21 19:21:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+client = discord.Client()
+client = commands.Bot(command_prefix='.')
+
+
+#add blacklisted emoji ids in here x
+bademojis = ['','']
+logchannelid = ''
+apikey = ''
+
+@client.event
+async def on_raw_reaction_add(reaction):
+    rudeuser = reaction.message_id
+    if reaction.event_type == 'REACTION_ADD':
+        if reaction.emoji in bademojis:
+            embed = discord.Embed(title='Found a dickhead',
+                                description=f"{rudeuser.mention} just added a blacklisted reaction:\n\n<:_:{reaction.emoji}>\n\nin <#{reaction.channel_id}>",
+                                colour=0x000000)
+
+            embed.set_footer(text="@syntaxszn • made with love")
+            logchannel = client.get_channel(logchannelid)
+            await logchannel.send(embed=embed)
+
+
+client.run()
